3901fa42d38fcd612a5e927ec2056e9d7241cfbe
Eric Do <ericdo.617@gmail.com>
2017-11-04 00:04:59 -0700

    set up drum pads

--- src/App.js
+++ src/App.js
@@ -6,13 +6,45 @@ class App extends Component {
   render() {
     return (
       <div className="App" id="drum-machine">
-        <div id="display">
-        
-
-        </div>
+        <div className="row">
+          <DrumContainer />
+          <ControlContainer />
+          </div>
       </div>
     );
   }
+}
+
+const DrumContainer = (props) => {
+  return(
+    <div id="drum-container" >
+      
+      <div className="row drum-row">
+          <button className="drum-pad">Q</button>
+          <button className="drum-pad">W</button>
+          <button className="drum-pad">E</button>
+      </div>
+      <div className="row drum-row">
+          <button className="drum-pad">A</button>
+          <button className="drum-pad">S</button>
+          <button className="drum-pad">D</button>
+      </div>
+      <div className="row drum-row">
+          <button className="drum-pad">Z</button>
+          <button className="drum-pad">X</button>
+          <button className="drum-pad">C</button>
+      
+      </div>
+  </div>
+  );
+
+}
+
+const ControlContainer = (props) => {
+  return(
+    <div id="display">
+    </div>
+  )
 }
 
 export default App;
