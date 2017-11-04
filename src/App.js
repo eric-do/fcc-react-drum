8967ed1b543049b8f351fdd722c84d76869552b8
Eric Do <ericdo.617@gmail.com>
2017-11-03 18:30:04 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" id="drum-machine">
+        <div id="display">
+        
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
